c9ab15f1adf7059e4a63003bfe5345c1545f7771
PoojaOffice <PoojaOffice@Pooja-J>
2018-05-17 16:52:03 +0530

    xczc

--- corejava/src/array/MissingNumberInArray.java
+++ corejava/src/array/MissingNumberInArray.java
@@ -1,6 +1,26 @@
 package array;
 
 public class MissingNumberInArray {
+	  public static void main(String[] args)
+	    {
+	        int n = 8;
+	 
+	        int[] a = {1, 4, 5, 3, 7, 8, 6};
+	 
+	        //Step 1
+	 
+	        int sumOfNnumbers = sumOfNnumbers(n);
+	 
+	        //Step 2
+	 
+	        int sumOfElements = sumOfElements(a);
+	 
+	        //Step 3
+	 
+	        int missingNumber = sumOfNnumbers - sumOfElements;
+	 
+	        System.out.println("Missing Number is = "+missingNumber);
+	    }
 	  //Method to calculate sum of 'n' numbers
 	 
     static int sumOfNnumbers(int n)
@@ -15,7 +35,7 @@ public class MissingNumberInArray {
     static int sumOfElements(int[] array)
     {
         int sum = 0;
- 
+      
         for (int i = 0; i < array.length; i++)
         {
             sum = sum + array[i];
@@ -24,24 +44,5 @@ public class MissingNumberInArray {
         return sum;
     }
  
-    public static void main(String[] args)
-    {
-        int n = 8;
- 
-        int[] a = {1, 4, 5, 3, 7, 8, 6};
- 
-        //Step 1
- 
-        int sumOfNnumbers = sumOfNnumbers(n);
- 
-        //Step 2
- 
-        int sumOfElements = sumOfElements(a);
- 
-        //Step 3
- 
-        int missingNumber = sumOfNnumbers - sumOfElements;
- 
-        System.out.println("Missing Number is = "+missingNumber);
-    }
+  
 }
