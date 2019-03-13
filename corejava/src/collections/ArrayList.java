6166e13377e16ec17aa5d2a00022899110f782e7
pooja.j <pooja.j@exilant.com>
2019-03-13 13:52:17 +0530

    test1

--- corejava/src/collections/ArrayList.java
+++ corejava/src/collections/ArrayList.java
@@ -3,6 +3,7 @@ package collections;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ArrayList {
@@ -15,32 +16,31 @@ public class ArrayList {
 		li.add(4);
 		li.add(5);
 		li.add(5);
-				System.out.println(li.stream().distinct().collect(Collectors.toList()));
+		System.out.println(li.stream().distinct().collect(Collectors.toList()));
 		System.out.println("occaorance of element java 7 " + Collections.frequency(li, 121));
 		System.out.println("occaorance of element java 8"
 				+ li.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting())));
-List l2 =  new java.util.ArrayList();
-		
-for(Object i:li) {
+		List l2 = new java.util.ArrayList();
 
-	if((Integer)i%2==0) {
-		l2.add(i);
+		for (Object i : li) {
+
+			if ((Integer) i % 2 == 0) {
+				l2.add(i);
+			}
+		}
+		System.out.println(l2);
+
+		li.stream().distinct().filter(n -> (Integer) n % 2 == 1).forEach(System.out::println);
+		List<Integer> l3 = (List<Integer>) li.stream().map(n -> (Integer) n * 5).collect(Collectors.toList());
+		System.out.println(l3);
+		li.stream().distinct().forEach(System.out::println);
+		List<Integer> q = l3.stream().map(Integer::valueOf).collect(Collectors.toList());
+		System.out.println(q);
+		Set l7 = (Set) li.stream().filter(n -> li.stream().filter(x -> x.equals(n)).count() > 1)
+				.collect(Collectors.toSet());
+		l3.stream().limit(3).forEach(System.out::println);
+		System.out.println(l7);
+
 	}
-	}
-System.out.println(l2);
 
-List l=(List) li.stream().distinct().filter(n -> (Integer)n % 2 == 1).collect(Collectors.toList());
-List<Integer> l3=(List<Integer>) li.stream().map(n->(Integer)n*5).collect(Collectors.toList());
-System.out.println(l3);
-li.stream().distinct().forEach(System.out::println);
-
-
-Set l7=(Set) li.stream().filter(n -> li.stream().filter(x -> x.equals(n)).count() > 1).collect(Collectors.toSet());
-
-System.out.println(l7);
-
-	
-	}
-	
-	
 }
